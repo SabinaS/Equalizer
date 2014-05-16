43cdb2ee0e078374643c0c9057d9031519fad746
SabinaS <SabinaS@users.noreply.github.com>
2014-05-16 07:52:15 -0400

    Update equalizer.c
    ffttest.c

--- equalizer.c
+++ equalizer.c
@@ -1,76 +1,92 @@
-//takes in 8k samples from fft_driver, equalizes them, passes them back to fft_driver
-
 #include <stdio.h>
-#include <unistd.h>
+#include <math.h>
+#include <complex.h>									//This library is declared before fftw3.h
+#include <fftw3.h>
 #include <stdint.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <string.h>
-#include <time.h>
-#include <stdlib.h>
-#include "fft_driver.h"
+#include <errno.h>
 
-#define FFT_DRIVER_WRITE_DIGIT _IOW(FFT_DRIVER_MAGIC, 1, u16 *)
-#define FFT_DRIVER_READ_DIGIT  _IOWR(FFT_DRIVER_MAGIC, 2, u16 *)
+#define CPU_AUDIO_US
+#include "cpu_audio.h"
 
-#define DRIVER_NAME "fft_driver"
-#define SAMPLENUM 8192
-#define SAMPLEBYTES SAMPLENUM*2
+#define SAMPLE_RATE 44100
 
-int fp; 
+static int slot_values[12] = {31, 72, 150, 250, 440, 630, 1000, 2500, 5000, 8000, 14000, 20000};
 
-void read_samples()
+int main(void)
 {
-    u16 *dataArray = kmalloc(SAMPLEBYTES, GFP_KERNEL); //allocating space for data array 
-    
-    if (ioctl(fd, FFT_DRIVER_READ_DIGIT, dataArray) == -1)
-        printf("FFT_DRIVER_READ_DIGIT failed: %s\n",
-            strerror(errno));
-    else {
-        if (status & FFT_DRIVER_READ_DIGIT)
-            puts("FFT_DRIVER_READ_DIGIT is not set");
-        else
-            puts("FFT_DRIVER_READ_DIGIT is set");
-    }
-    
-}
+	int i;
+	struct sample *cpu_in; 
+	int Npoints = SAMPLENUM;
+	fftw_complex *in, *out, *reverse;
+	fftw_plan plan, reverse_plan;
 
-void write_samples(u16* dataArray)
-{
-    if (ioctl(fd, FFT_DRIVER_WRITE_DIGIT, dataArray) == -1)
-        printf("FFT_DRIVER_WRITE_DIGIT failed: %s\n",
-            strerror(errno));
-    else {
-        if (status & FFT_DRIVER_READ_DIGIT)
-            puts("FFT_DRIVER_WRITE_DIGIT is not set");
-        else
-            puts("FFT_DRIVER_WRITE_DIGIT is set");
-    }
-}
+	in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*Npoints);			/*allocating memory*/
+	out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*Npoints);		/*allocating memory */
+	reverse = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*Npoints);
+	cpu_in = (struct sample*)fftw_malloc(sizeof(struct sample) * Npoints); 
+	plan = fftw_plan_dft_1d(Npoints, in, out, FFTW_FORWARD, FFTW_ESTIMATE); 	/*Here we set which kind of transformation we want to perform */
+	reverse_plan = fftw_plan_dft_1d(Npoints, out, reverse, FFTW_BACKWARD, FFTW_ESTIMATE);
+	
+	/*read in from cpu_audio */
+	/*convert to fftw_complex in */
+	 char *audfile = "/dev/cpu_audio";
+	 int aud_file_desc; 
+	 if ((aud_file_desc = open(audfile, O_RDWR)) == -1 ) {
+        	fprintf(stderr, "could not open %s\n", audfile);
+        	return -1;
+      	 }
+	 char *visfile = "/dev/visualizer"; 
+	 int vis_file_desc;
+	 if ((vis_file_desc = open(visfile, O_RDWR)) == -1 ) {
+                fprintf(stderr, "could not open %s\n", visfile);
+                return -1;
+         } 
+	
+	 /* read in values from cpu_audio via icotl*/
+	 while (ioctl(aud_file_desc, CPU_AUDIO_READ_SAMPLES, cpu_in)) {
+            	perror("ioctl read failed!");
+	    	if( errno == EAGAIN){
+			 usleep(1/60.0 * SAMPLENUM);
+             continue;
+		}
+		else{		
+            		close(aud_file_desc);
+            		return -1;
+		}
+     	 }
 
-int main()
-{
-    
-    static const char *filename = "/dev/vga_ball"; //which file?
-    u16 *dataArray = kmalloc(SAMPLEBYTES, GFP_KERNEL); //allocating space for data array
-    
-    if ((fp = open(filename, O_RDWR)) == -1) {
-        fprintf(stderr, "could not open %s\n", filename);
-        return -1;
-    }
-    
-    read_samples(); //reading in 32k samples 
-    
-    //equalize samples 
-    fft(dataArray); 
-    //0 to SAMPLENUM; get values from hardware
-    //STD_LOGIC_VECTOR(WIDTH_EXPONENT-1 DOWNTO 0)
-    
-    write_samples(dataArray); //writing the 32k samples 
-    
-    close(fd);
-    printf("Equalizer Userspace program terminating\n");
-    return 0;
+	printf("\nCoefficcients of the expansion:\n\n");
+	for(i = 0; i < Npoints/2; i++)
+	{
+		in[1] = cpu_n[i].left; 		
+		//in[i] = i + 0 * I;
+		// printf("%d %11.7f %11.7f\n", i, creal(in[i]), cimag(in[i]));		//creal and cimag are functions of complex.h 
+	}
+	printf("\n");
+
+	fftw_execute(plan); 								//Execution of FFT
+
+	printf("Output Amplitude:\n\n");
+	for(i = 0; i < Npoints; i++)
+	{
+		printf("%d %f\n", i, 10 * log(creal(out[i]) * creal(out[i]) + cimag(out[i]) * cimag(out[i])));
+	}
+
+	fftw_execute(reverse_plan); 								//Execution of FFT
+
+	printf("Reverse output:\n\n");
+	for(i = 0; i < Npoints; i++)
+	{
+		// printf("%d %11.7f %11.7f\n", i, creal(reverse[i]) / Npoints, cimag(reverse[i]));
+	}
+
+	
+	fftw_destroy_plan(plan);							//Destroy plan
+	fftw_free(in);			 						//Free memory
+	fftw_free(out);			 						//Free memory
+	fftw_destroy_plan(reverse_plan);							//Destroy plan
+	fftw_free(reverse);			 						//Free memory
+	return 0;
 }
